5e9625330c7fb9732de7af7d21622f18653660cf
Shahidulislam34 <shahidulislam159854@gmail.com>
2024-07-01 21:25:10 +0600

    ADD

--- 1.Codeforces/EPIC Institute of Technology Round Summer 2024 (Div. 1 + Div. 2)/A.cpp
+++ 1.Codeforces/EPIC Institute of Technology Round Summer 2024 (Div. 1 + Div. 2)/A.cpp
@@ -5,25 +5,9 @@ using namespace std;
 #define endl '\n'
 
 void sol(){
-    int n;
-    cin >> n;
-    vector<int>v(n + 5);
-    for (int i = 1; i <= n; ++i) cin >> v[i];
-    vector<int>tmp;
-    int mx = v[1];
-    for (int i = 2; i <= n; ++i){
-        if (v[i] >= mx) mx = v[i];
-        else tmp.push_back(mx - v[i]);
-    }
-    sort(tmp.begin(), tmp.end());
-    int res = 0, sum = 0;
-    n = tmp.size();
-    for (int i = 0; i < n; ++i){
-        if (tmp[i] - sum > 0){
-            res += (n - i + 1) * (tmp[i] - sum);
-            sum += (tmp[i] - sum);
-        }
-    }
+    int n, k;
+    cin >> n >> k;
+    int res = ((max(0ll, n - 1) * k) + 1);
     cout << res << endl;
 }
 
@@ -35,3 +19,4 @@ int32_t main(){
     while (tt--) sol();
     return 0;
 }
+
--- 1.Codeforces/EPIC Institute of Technology Round Summer 2024 (Div. 1 + Div. 2)/B.cpp
+++ 1.Codeforces/EPIC Institute of Technology Round Summer 2024 (Div. 1 + Div. 2)/B.cpp
@@ -5,9 +5,25 @@ using namespace std;
 #define endl '\n'
 
 void sol(){
-    int n, k;
-    cin >> n >> k;
-    int res = ((max(0ll, n - 1) * k) + 1);
+    int n;
+    cin >> n;
+    vector<int>v(n + 5);
+    for (int i = 1; i <= n; ++i) cin >> v[i];
+    vector<int>tmp;
+    int mx = v[1];
+    for (int i = 2; i <= n; ++i){
+        if (v[i] >= mx) mx = v[i];
+        else tmp.push_back(mx - v[i]);
+    }
+    sort(tmp.begin(), tmp.end());
+    int res = 0, sum = 0;
+    n = tmp.size();
+    for (int i = 0; i < n; ++i){
+        if (tmp[i] - sum > 0){
+            res += (n - i + 1) * (tmp[i] - sum);
+            sum += (tmp[i] - sum);
+        }
+    }
     cout << res << endl;
 }
 
@@ -19,14 +35,3 @@ int32_t main(){
     while (tt--) sol();
     return 0;
 }
-
-
-
-
-
-
-
-
-
-
-
